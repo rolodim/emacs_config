8ecdc44289c47b81a49b65579824c106f7d0aa4f
rolodim <omega.snd@gmail.com>
2021-04-15 23:26:05 +0300

    Changed the theme and installed ivy, helpful

--- init.el
+++ init.el
@@ -1,7 +1,112 @@
-(setq
- ;; use gdb-many-windows by default
- gdb-many-windows t
+(setq inhibit-startup-message t)
 
- ;; Non-nil means display source file containing the main routine at startup
- gdb-show-main t
+(scroll-bar-mode -1)              ; Disable visible scrollbar
+(tool-bar-mode -1)                ; Disable the toolbar
+(tooltip-mode -1)                 ; Disable tooltips
+(set-fringe-mode 10)              ; Don't know what it does
+
+(menu-bar-mode -1)                ; Disable menu bar
+
+(setq visible-bell t)             ; Set up the visible bell
+
+(set-face-attribute 'default nil :font "Ubuntu Mono" :height 120)
+
+(load-theme 'wombat)
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;;Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (dispaly-line-numbers-mode 0))))
+
+
+(use-package ivy
+  :bind (("C-s" . swiper)
+	  :map ivy-minibuffer-map
+	  ("TAB" . ivy-alt-done)
+	  ("C-l" . ivy-alt-done)
+	  ("C-n" . ivy-next-line)
+	  ("C-p" . ivy-previous-line)
+	  :map ivy-switch-buffer-map
+	  ("C-p" . ivy-previous-line)
+	  ("C-l" . ivy-done)
+	  ("C-d" . ivy-switch-buffer-kill)
+	  :map ivy-reverse-i-search-map
+	  ("C-p" . ivy-previous-line)
+	  ("C-d" . ivy-reverse-i-search-kill))
+  :init
+  (ivy-mode 1))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 12))
+  )
+
+(use-package doom-themes)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (doom-themes which-key use-package rainbow-delimiters ivy-rich helpful doom-modeline counsel))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
