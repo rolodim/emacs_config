54c1273e13820bbb9c0780527ce4d1b5dd3e760f
rolodim <omega.snd@gmail.com>
2021-04-20 13:40:17 +0300

    * emacs.org: pre-working config with broken company-mode and yas

--- init.el
+++ init.el
@@ -53,21 +53,68 @@
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "C-z") nil)
+
+;; M- arrows to move focus between buffers
+(windmove-default-keybindings 'meta)
+
+;; (use-package dashboard
+    ;;   :ensure t
+    ;;   :config
+    ;;   (dashboard-setup-startup-hook))
+
+  (use-package dashboard
+    :diminish (dashboard-mode page-break-lines-mode)
+    :custom
+    (dashboard-banner-logo-title "Close the world. Open the nExt.")
+    (dashboard-items '((recents  . 7)
+                       (bookmarks . 7)
+                       (agenda . 5)))
+
+    (dashboard-set-heading-icons t)
+    (dashboard-set-navigator t)
+    (dashboard-navigator-buttons
+     (if (featurep 'all-the-icons)
+         `(((,(all-the-icons-octicon "mark-github" :height 1.1 :v-adjust -0.05)
+             "M-EMACS" "Browse M-EMACS Homepage"
+             (lambda (&rest _) (browse-url "https://github.com/MatthewZMD/.emacs.d")))
+            (,(all-the-icons-fileicon "elisp" :height 1.0 :v-adjust -0.1)
+             "Configuration" "" (lambda (&rest _) (edit-configs)))
+            (,(all-the-icons-faicon "cogs" :height 1.0 :v-adjust -0.1)
+             "Update" "" (lambda (&rest _) (auto-package-update-now)))))
+       `((("" "M-EMACS" "Browse M-EMACS Homepage"
+           (lambda (&rest _) (browse-url "https://github.com/MatthewZMD/.emacs.d")))
+          ("" "Configuration" "" (lambda (&rest _) (edit-configs)))
+          ("" "Update" "" (lambda (&rest _) (auto-package-update-now)))))))
+      :config
+    (dashboard-setup-startup-hook)
+    (dashboard-modify-heading-icons '((recents . "file-text")
+                                      (bookmarks . "book"))))
+
+(setq dashboard-startup-banner 'logo)
 
 (use-package doom-themes
-  :init (load-theme 'doom-palenight t))
+  :init (load-theme 'doom-palenight t)
+  (doom-themes-visual-bell-config))
 
 (use-package all-the-icons)
-;; Run M-x all-the-icons-install-fonts to display doom-modeline correctly
+   ;; Run M-x all-the-icons-install-fonts to display doom-modeline correctly
 
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 12))
-  )
+ (use-package minions
+   :hook (doom-modeline-mode . minions-mode))
 
-(set-frame-parameter (selected-frame) 'alpha '(92 . 90))
-(add-to-list 'default-frame-alist '(alpha . (92 . 90)))
+ (use-package doom-modeline
+   :ensure t
+   :init (doom-modeline-mode 1)
+   :custom
+   (doom-modeline-height 12)
+   (doom-modeline-bar-width 6)
+   (doom-modeline-lsp t)
+   (doom-modeline-minor-modes t)
+   )
+
+   (set-frame-parameter (selected-frame) 'alpha '(92 . 90))
+   (add-to-list 'default-frame-alist '(alpha . (92 . 90)))
 
 (use-package which-key
   :init (which-key-mode)
@@ -96,7 +143,9 @@
 
 (use-package ivy-rich
   :init
-  (ivy-rich-mode 1))
+  (ivy-rich-mode 1)
+  :config
+  (ivy-rich-project-root-cache-mode t))
 
 (use-package counsel
 :bind (
@@ -125,6 +174,20 @@
   ([remap describe-command] . helpful-command)
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
+
+(use-package undo-tree
+  :defer t
+  :diminish undo-tree-mode
+  :init (global-undo-tree-mode)
+  :custom
+  (undo-tree-visualizer-diff t)
+  (undo-tree-visualizer-timestamps t))
+
+(use-package psession
+:config
+(psession-mode 1)
+(psession-autosave-mode 1)
+ )
 
 (defun efs/org-font-setup ()
   ;; Replace list hyphen with dot
@@ -163,6 +226,8 @@
   (setq org-ellipsis " ▾")
   (efs/org-font-setup))
 
+(setq org-support-shift-select 'always)
+
 (use-package org-bullets
   :after org
   :hook (org-mode . org-bullets-mode)
@@ -194,12 +259,60 @@
 
 (push '("config-unix" . conf-unix) org-src-lang-modes)
 
-(require 'org-tempo)
-
 (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
 (add-to-list 'org-structure-template-alist '("py" . "src python"))
 
+(setq c-default-style "java")
 
+(use-package quickrun
+    :bind
+    (("<f5>" . quickrun)
+     ("M-<f5>" . quickrun-shell)
+     ("C-c e" . quickrun)
+     ("C-c C-e" . quickrun-shell)))
+
+(setq quickrun-timeout-seconds -1)
+
+      (global-set-key (kbd "<f6>") (lambda ()
+                                     (interactive)
+                                     (setq-local compilation-read-command nil)
+                                     (call-interactively 'compile)))
+
+    (setq-default compilation-always-kill t) ; kill compilation process before starting another
+
+    (setq-default compilation-ask-about-save nil) ; save all buffers on `compile'
+
+    (setq-default compilation-scroll-output t)
+
+   (defun cc-mode-compile-clean ()
+     (interactive)
+     (setq compile-command "cd ../ && make clean")
+     (call-interactively 'compile))
+   (defun cc-mode-compile ()
+     (local-set-key (kbd "C-x C c") 'cc-mode-compile-clean))
+
+   (add-hook 'c-mode-hook 'cc-mode-compile)
+   (add-hook 'c++-mode-hook 'cc-mode-compile)
+
+(setq
+ ;; use gdb-many-windows by default
+ gdb-many-windows t
+ ;; Non-nil means display source file containing the main routine at startup
+ gdb-show-main t
+ )
+
+ (use-package dap-mode
+  :diminish
+  :config
+  (require 'dap-cpptools)               
+      :bind
+      (:map dap-mode-map
+            (("<f12>" . dap-debug)
+             ("<f8>" . dap-continue)
+             ("<f9>" . dap-next)
+             ("<M-f11>" . dap-step-in)
+             ("C-M-<f11>" . dap-step-out)
+             ("<f7>" . dap-breakpoint-toggle))))
 
 (use-package lsp-mode
     ;;:straight t
@@ -210,7 +323,7 @@
     (lsp-auto-guess-root nil)
     (lsp-file-watch-threshold 2000)
     (read-process-output-max (* 1024 1024))
-    :hook (((c-mode c++-mode objc-mode) . lsp)
+    :hook (((c-mode c++-mode objc-mode) . lsp-deferred)
            (lsp-mode . lsp-enable-which-key-integration)
            (lsp-mode . lsp-diagnostics-modeline-mode))
     :bind ("C-c C-c" . #'lsp-execute-code-action)
@@ -232,6 +345,34 @@
   (use-package lsp-ivy
       :after (ivy lsp-mode))
 ;
+
+(electric-pair-mode 1)
+
+(use-package highlight-indent-guides
+  :custom
+  (highlight-indent-guides-responsive 'top)
+  (highlight-indent-guides-delay 0)
+  (highlight-indent-guides-auto-character-face-perc 7))
+(setq highlight-indent-guides-method 'bitmap)
+(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+
+(setq-default indent-tabs-mode nil)
+ (setq-default indent-line-function 'insert-tab)
+ (setq-default tab-width 4)
+ (setq-default c-basic-offset 4)
+ (setq-default js-switch-indent-offset 4)
+ (c-set-offset 'comment-intro 0)
+ (c-set-offset 'innamespace 0)
+ (c-set-offset 'case-label '+)
+ (c-set-offset 'access-label 0)
+ (c-set-offset (quote cpp-macro) 0 nil)
+ (defun smart-electric-indent-mode ()
+   "Disable 'electric-indent-mode in certain buffers and enable otherwise."
+   (cond ((and (eq electric-indent-mode t)
+               (member major-mode '(erc-mode text-mode)))
+          (electric-indent-mode 0))
+         ((eq electric-indent-mode nil) (electric-indent-mode 1))))
+ (add-hook 'post-command-hook #'smart-electric-indent-mode)
 
 (use-package projectile
   :diminish projectile-mode
@@ -255,24 +396,58 @@
   :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package company
-      :diminish
-      :after lsp-mode
-      :hook (prog-mode . company-mode)
-      :bind (:map company-active-map
-            ("<tab>" . company-complete-selection))
-            (:map lsp-mode-map
-             ("<tab>" . company-indent-or-complete-common))
-      :custom
-      (company-minimum-prefix-length 1)
-      (company-idle-delay 0.0))
-
-(use-package company-box
   :diminish
-  :defines company-box-icons-all-the-icons
-  :hook (company-mode . company-box-mode)
+  :after lsp-mode
+  :bind (:map company-active-map
+              ("<tab>" . company-complete-selection))
+              (:map lsp-mode-map
+               ("<tab>" . company-indent-or-complete-common))
+
+  :hook (prog-mode . company-mode))
   :custom
-  (company-box-backends-colors nil)
-  )
+  (company-dabbrev-downcase nil "Don't downcase returned candidates.")
+  (company-show-numbers t "Numbers are helpful.")
+  (company-tooltip-limit 20 "The more the merrier.")
+  (company-tooltip-idle-delay 0.4 "Faster!")
+  (company-minimum-prefix-length 1)
+  (company-async-timeout 20 "Some requests can take a long time. That's fine.")
+
+  :config
+  ;; Use the numbers 0-9 to select company completion candidates
+(let ((map company-active-map))
+  (mapc (lambda (x) (define-key map (format "%d" x)
+ `(lambda () (interactive) (company-complete-number ,x))))
+ (number-sequence 0 9))))
+
+
+
+      ;; (use-package company
+      ;;   :diminish
+      ;;   :after lsp-mode
+      ;;   :hook (prog-mode . company-mode)
+      ;;   :bind (:map company-active-map
+      ;;         ("<tab>" . company-complete-selection))
+      ;;         (:map lsp-mode-map
+      ;;          ("<tab>" . company-indent-or-complete-common))
+      ;;   :custom
+      ;;   (company-minimum-prefix-length 1)
+      ;;   (company-idle-delay 0.0))
+
+  ;; (use-package company-box
+  ;;   :diminish
+  ;;   :defines company-box-icons-all-the-icons
+  ;;   :hook (company-mode . company-box-mode)
+  ;;   :custom
+  ;;   (company-box-backends-colors nil)
+  ;;   )
+
+(use-package dumb-jump
+  :bind
+  (:map prog-mode-map
+        (("C-c C-o" . dumb-jump-go-other-window)
+         ("C-c C-j" . dumb-jump-go)
+         ("C-c C-i" . dumb-jump-go-prompt)))
+  :custom (dumb-jump-selector 'ivy))
 
 (use-package flycheck
   :defer t
@@ -295,4 +470,91 @@
 (use-package flycheck-inline
   :config (global-flycheck-inline-mode))
 
-(electric-pair-mode 1)
+;; (use-package yasnippet
+    ;;   :diminish yas-minor-mode
+    ;;   :init
+    ;;   (use-package yasnippet-snippets :after yasnippet)
+    ;;   :hook ((prog-mode LaTeX-mode org-mode) . yas-minor-mode)
+    ;;   :bind
+    ;;   (:map yas-minor-mode-map ("C-c C-n" . yas-expand-from-trigger-key))
+    ;;   (:map yas-keymap
+    ;;         (("TAB" . smarter-yas-expand-next-field)
+    ;;          ([(tab)] . smarter-yas-expand-next-field)))
+    ;;   :config
+    ;;    (yas-reload-all)
+    ;;   (defun smarter-yas-expand-next-field ()
+    ;;     "Try to `yas-expand' then `yas-next-field' at current cursor position."
+    ;;     (interactive)
+    ;;     (let ((old-point (point))
+    ;;           (old-tick (buffer-chars-modified-tick)))
+    ;;       (yas-expand)
+    ;;       (when (and (eq old-point (point))
+    ;;                  (eq old-tick (buffer-chars-modified-tick)))
+    ;;         (ignore-errors (yas-next-field))))))
+
+(use-package yasnippet
+  :defer 3 ;; takes a while to load, so do it async
+  :diminish yas-minor-mode
+  :config (yas-global-mode)
+  :custom (yas-prompt-functions '(yas-completing-prompt)))
+  (setq ac-source-yasnippet nil)
+
+(use-package treemacs
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :custom
+  (treemacs-collapse-dirs 3)
+  (treemacs-deferred-git-apply-delay 0.5)
+  (treemacs-display-in-side-window t)
+  (treemacs-file-event-delay 5000)
+  (treemacs-file-follow-delay 0.2)
+  (treemacs-follow-after-init t)
+  (treemacs-follow-recenter-distance 0.1)
+  (treemacs-git-command-pipe "")
+  (treemacs-goto-tag-strategy 'refetch-index)
+  (treemacs-indentation 2)
+  (treemacs-indentation-string " ")
+  (treemacs-is-never-other-window nil)
+  (treemacs-max-git-entries 5000)
+  (treemacs-no-png-images nil)
+  (treemacs-no-delete-other-windows t)
+  (treemacs-project-follow-cleanup nil)
+  (treemacs-persist-file (expand-file-name ".cache/treemacs-persist" user-emacs-directory))
+  (treemacs-recenter-after-file-follow nil)
+  (treemacs-recenter-after-tag-follow nil)
+  (treemacs-show-cursor nil)
+  (treemacs-show-hidden-files t)
+  (treemacs-silent-filewatch nil)
+  (treemacs-silent-refresh nil)
+  (treemacs-sorting 'alphabetic-desc)
+  (treemacs-space-between-root-nodes t)
+  (treemacs-tag-follow-cleanup t)
+  (treemacs-tag-follow-delay 1.5)
+  (treemacs-width 35)
+  :config
+  ;; The default width and height of the icons is 22 pixels. If you are
+  ;; using a Hi-DPI display, uncomment this to double the icon size.
+  ;;(treemacs-resize-icons 44)
+  (treemacs-follow-mode t)
+  (treemacs-filewatch-mode t)
+  (treemacs-fringe-indicator-mode t)
+  :bind
+  (("M-0"       . treemacs-select-window)
+   ("C-x t 1"   . treemacs-delete-other-windows)
+   ("C-x t t"   . treemacs)
+   ("C-x t B"   . treemacs-bookmark)
+   ("C-x t C-t" . treemacs-find-file)
+   ("C-x t M-t" . treemacs-find-tag))
+  (:map treemacs-mode-map ("C-p" . treemacs-previous-line)))
+
+  (use-package lsp-treemacs
+    :after lsp)
+
+  (use-package treemacs-magit
+    :defer t
+    :after (treemacs magit))
+
+  (use-package treemacs-projectile
+    :defer t
+    :after (treemacs projectile))
